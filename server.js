34ac45efd7d8136de05d4789d67aba3159c151e2
Malcolm McKeown <malkey@gmail.com>
2016-01-20 04:13:06 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict';
+const express = require("express");
+const strftime = require("strftime");
+
+const app = express();
+
+
+app.get("/:date", (req, res) => {
+  // debugger;
+  req.acceptsLanguages();
+  const dateString = req.params.date;
+  if (dateString === "favicon.ico")
+    return res.sendStatus(404);
+    
+  const date = new Date(dateString);
+  
+  let dateJSON = {
+    unix: null,
+    natural: null
+  };
+  
+  if (!Number.isNaN(date.getTime())) {
+    dateJSON.unix = date.getTime();
+    dateJSON.natural = strftime("%B %e, %Y", date);
+  }
+  
+  res.json(dateJSON);
+});
+
+const listener = app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on ${listener.address().port}`);
+});
